375aa77daee61d6ad033aabb379ecbd5cfbb9bcd
Aram Koukia <aramkoukia@gmail.com>
2017-03-07 15:49:16 -0800

    added dynamic form service to read the data from json file

---
+++ src/app/services/dynamic-form.service.ts
@@ -0,0 +1,29 @@
+﻿import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http'
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+
+export class DynamicFormsService {
+
+    result: any;
+    error: any;
+
+    http: Http;
+    constructor(http: Http) {
+        this.http = http;
+    }
+
+    GetFormTemplates(): Object {
+        this.http.get('./TemplateNames.json').map((res: Response) => res.json()).subscribe(res => this.result = res);
+        return this.result;
+    }
+
+    GetFormTemplate(templateName: string): Object {
+        this.http.get('./' + templateName + '.json')
+            .map((res: Response) => res.json())
+            .subscribe(res => this.result = res, error => this.error = error);
+        return this.result;
+    }
+
+
+}
