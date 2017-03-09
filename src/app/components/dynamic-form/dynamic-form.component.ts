a411b70602c3448ae8c9eb1af672cf5a116317ec
ARAM KOUKIA <ARAM KOUKIA>
2017-03-08 17:11:35 -0800

    some minor fixes

--- src/app/components/dynamic-form/dynamic-form.component.ts
+++ src/app/components/dynamic-form/dynamic-form.component.ts
@@ -2,6 +2,8 @@
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Question } from './../../models';
 import { DynamicFormService } from './../../services/dynamic-form.service';
+import { Observable } from 'rxjs/Observable';
+import { Http, Response } from '@angular/http'
 
 @Component({
     selector: 'dynamic-form',
@@ -9,22 +11,21 @@ import { DynamicFormService } from './../../services/dynamic-form.service';
 })
 export class DynamicFormComponent implements OnInit {
 
-    @Input() questions: Array<Question>;
+    @Input() questions: Question[];
     errorMessage: string;
     formGroup: FormGroup;
     payload: string
 
     constructor(private dynamicFormService: DynamicFormService) {
 
-    }
-
-    ngOnInit() {
-
         this.dynamicFormService.GetFormTemplate()
                         .subscribe(
                             questions => this.questions = questions,
                             error =>  this.errorMessage = <any>error);
                             
+    }
+
+    ngOnInit() {
 
         this.formGroup = this.generateForm(this.questions);
 
--- src/app/services/dynamic-form.service.ts
+++ src/app/services/dynamic-form.service.ts
@@ -10,18 +10,66 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class DynamicFormService {
 
-    result : Array<Question>;
-    array : Array<any>;
-    loading: boolean;
     constructor(private http: Http) {
-        // this.http.get('./app/services/template1.json')
-        //     .map(response => response.json())
-        //     .subscribe(result => this.result = result);
-            
-        //     // http.get('friends.json')
-        //     // .subscribe(result => this.result =result.json());
+    }
 
-        // console.log(this.result);
+    GetFormTemplate(/*templateName: string*/) : Observable<Question[]> {
+        //TODO: fix the error here: this.result is undefined...
+
+            return this.http.get('./app/services/template1.json')
+                        .map(this.extractData)
+                        .catch(this.handleError);
+
+        // return [
+        //    {
+        //        "controlType": "text-input",
+        //        "id": "asdasd",
+        //        "label": "My First",
+        //        "required": false,
+        //        "options": null,
+        //        "type": "text",
+        //        "value": "test"
+        //    },
+        //    {
+        //        "controlType": "textarea",
+        //        "id": "first",
+        //        "label": "My First",
+        //        "required": false,
+        //        "options": null,
+        //        "type": "text",
+        //        "value": "test"
+        //    },
+        //    {
+        //        "controlType": "text",
+        //        "id": "second",
+        //        "label": "Second!",
+        //        "required": true,
+        //        "options": null,
+        //        "type": "text",
+        //        "value": "test"
+        //    }
+        // ];
+
+    }
+
+    private extractData(res: Response) {
+        let body = res.json();
+        return body || { };
+    }
+
+    private handleError (error: Response | any) {
+        // In a real world app, we might use a remote logging infrastructure
+        let errMsg: string;
+        if (error instanceof Response) {
+            const body = error.json() || '';
+            const err = body.error || JSON.stringify(body);
+            errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+        } 
+        else {
+            errMsg = error.message ? error.message : error.toString();
+        }
+        console.error(errMsg);
+        return Observable.throw(errMsg);
     }
 
     GetFormTemplates(): Array<Question> {
@@ -57,64 +105,6 @@ export class DynamicFormService {
         //TODO: fix the error here: this.result is undefined...
         //this.http.get('./TemplateNames.json').map((res: Response) => res.json()).subscribe(res => this.result = res);
         //return this.result;
-    }
-
-    GetFormTemplate(/*templateName: string*/) : Observable<Question[]> {
-
-        // return [
-        //    {
-        //        "controlType": "text-input",
-        //        "id": "asdasd",
-        //        "label": "My First",
-        //        "required": false,
-        //        "options": null,
-        //        "type": "text",
-        //        "value": "test"
-        //    },
-        //    {
-        //        "controlType": "textarea",
-        //        "id": "first",
-        //        "label": "My First",
-        //        "required": false,
-        //        "options": null,
-        //        "type": "text",
-        //        "value": "test"
-        //    },
-        //    {
-        //        "controlType": "text",
-        //        "id": "second",
-        //        "label": "Second!",
-        //        "required": true,
-        //        "options": null,
-        //        "type": "text",
-        //        "value": "test"
-        //    }
-        // ];
-        //TODO: fix the error here: this.result is undefined...
-
-
-        return this.http.get('./app/services/template1.json')
-                    .map(this.extractData)
-                    .catch(this.handleError);
-        }
-
-        private extractData(res: Response) {
-            let body = res.json();
-            return body.data || { };
-        }
-        private handleError (error: Response | any) {
-            // In a real world app, we might use a remote logging infrastructure
-            let errMsg: string;
-            if (error instanceof Response) {
-            const body = error.json() || '';
-            const err = body.error || JSON.stringify(body);
-            errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-            } else {
-            errMsg = error.message ? error.message : error.toString();
-            }
-            console.error(errMsg);
-            return Observable.throw(errMsg);
-        }
-
+    }    
 
 }
